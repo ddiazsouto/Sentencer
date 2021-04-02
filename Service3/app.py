016ad1710d1fb951b33af87617c5f1b60eb6f0ff
Daniel <destruktor@master.us-central1-a.c.peppy-castle-305110.internal>
2021-04-02 15:31:49 +0000

    Finished all the Dockerfiles

--- Service3/app.py
+++ Service3/app.py
@@ -6,4 +6,4 @@ def number():
     return 3
 
 if __name__ == '__main__':
-    app.run(port=5005, debug=True)
+    app.run(port=5005, host='0.0.0.0', debug=True)
--- Service4/app.py
+++ Service4/app.py
@@ -6,4 +6,4 @@ def number():
     return 4
 
 if __name__ == '__main__':
-    app.run(port=5050, debug=True)
+    app.run(port=5050, host='0.0.0.0', debug=True)
