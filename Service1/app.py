81b0a769216eca3dff4e300467f0cb3111add17a
Daniel <ddiazsouto@gmail.com>
2021-04-02 10:28:14 +0000

    Have the app for the 4 Services started

--- Service1/app.py
+++ Service1/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/number', methods=['GET'])
 def number():
-    return 3
+    return 1
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(port=5500, debug=True)
---
+++ Service3/app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/number', methods=['GET'])
+def number():
+    return 3
+
+if __name__ == '__main__':
+    app.run(port=5005, debug=True)
