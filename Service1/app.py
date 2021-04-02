d03e19e8ed1c8ca9b7ff961f33e8605787749e92
Daniel <destruktor@master.us-central1-a.c.peppy-castle-305110.internal>
2021-04-02 17:05:17 +0000

    and the fun starts

--- Service1/app.py
+++ Service1/app.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/number', methods=['GET'])
 def number():
-    return 1
+    template='main.html'
+    return render_template(template, title='Frontend')
 
 if __name__ == '__main__':
     app.run(port=5500, host='0.0.0.0', debug=True)
