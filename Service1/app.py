9e0ee611c06f92750a034efcaba6c7eb303cede3
Daniel <destruktor@master.us-central1-a.c.peppy-castle-305110.internal>
2021-04-03 06:42:38 +0000

    built a sketch of a POST accepting app

--- Service1/app.py
+++ Service1/app.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, Response, jsonify
 app = Flask(__name__)
 
-@app.route('/number', methods=['GET'])
+some={'dan':'Cool', 'Other': 'not so cool'}
+
+
+@app.route('/', methods=['GET', 'POST'])
 def number():
     template='main.html'
-    return render_template(template, title='Frontend')
+    color='blue'
+
+
+    if request.method == 'POST':
+
+        data_received = request.data.decode('utf-8')      
+        return Response(data_received)
+
+    return render_template(template, title='Frontend', color=color)
 
 if __name__ == '__main__':
     app.run(port=5500, host='0.0.0.0', debug=True)
