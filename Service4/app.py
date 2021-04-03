6297a230873b852dd8e499ae066d4c2fb8690b11
Daniel <destruktor@master.us-central1-a.c.peppy-castle-305110.internal>
2021-04-03 06:44:43 +0000

    added something to start working in Service 4

--- Service4/app.py
+++ Service4/app.py
@@ -3,6 +3,10 @@ app = Flask(__name__)
 
 @app.route('/number', methods=['GET'])
 def number():
+
+    response = requests.post(api + '/post/text', 'My Data')
+    print('Response: ', response.text)
+
     return 4
 
 if __name__ == '__main__':
