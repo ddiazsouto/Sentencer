db54c73dedfceaa5ae43c55e7423fd6515429aef
Daniel <destruktor@master.us-central1-a.c.peppy-castle-305110.internal>
2021-04-05 15:16:28 +0000

    Trying to Jsonify to do something cool

--- Service4/app.py
+++ Service4/app.py
@@ -8,16 +8,21 @@ some={'dan':'Cool', 'Other': 'not so cool'}
 @app.route('/', methods=['GET', 'POST'])
 def middleend():
 
+    output = dict()
+
     response = requests.get('http://0.0.0.0:5000/')  #  http://35.223.236.160:5000/
     sentence = requests.get('http://0.0.0.0:5005/') # http://35.223.236.160:5005/
+
+    output[response] = sentence
+
+
 
     # if request.method == 'POST':
 
     #     data_received = request.data.decode('utf-8')      
     #     return Response(data_received)
 
-    return f'Hey, I am talking with Service 2, and its saying: {response.text}\
-        <br><br> am also talking with Service3 and it is saying: {sentence.text}'
+    return f'{output}'
 
 if __name__ == '__main__':
     app.run(port=5050, host='0.0.0.0', debug=True)
