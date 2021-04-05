41143f34e82cf4353536cd0f5fce71756541f336
Daniel <destruktor@master.us-central1-a.c.peppy-castle-305110.internal>
2021-04-05 15:36:35 +0000

    increased the variety of outputs

--- Service3/app.py
+++ Service3/app.py
@@ -7,7 +7,7 @@ def randomization():
     sentences = ['It was the craziest thing that my eyes have seen', 'How great', 'Could be better', 'Holly Cow!!', \
     'Awesome', 'Like a fish in the water', 'Who cares?', 'Yeeah baby!!', 'Not bad at all', 'Fantastic!', 'Nice!']
 
-    select = randint(0, 6)
+    select = randint(0, 10)
 
     return str(sentences[select])
 
