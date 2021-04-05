4d4c07ec76a517c3f3b28c6ef04c5017ad0e456a
Daniel <destruktor@master.us-central1-a.c.peppy-castle-305110.internal>
2021-04-05 13:40:39 +0000

    updated Service2

--- Service2/app.py
+++ Service2/app.py
@@ -1,9 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/number', methods=['GET'])
-def number():
-    return 2
+@app.route('/', methods=['GET'])
+def randomizer():
+    return '2'
 
 if __name__ == '__main__':
     app.run(port=5000, host='0.0.0.0', debug=True)
