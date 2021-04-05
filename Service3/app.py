d032258cda347e798f0dd2aad7c89fdf5948ef76
Daniel <destruktor@master.us-central1-a.c.peppy-castle-305110.internal>
2021-04-05 14:12:37 +0000

    Services 3 and 2 generate random elements, numbers and expressions, Updated

--- Service3/app.py
+++ Service3/app.py
@@ -1,9 +1,16 @@
 from flask import Flask
+from random import randint
 app = Flask(__name__)
 
 @app.route('/number', methods=['GET'])
 def randomization():
-    return 3
+    sentences = ['How great', 'Could be better', 'Holly Cow!!', 'Awesome', 'Like a fish in the water', 'Who cares?']
+
+    select = randint(1, 6)
+
+
+
+    return sentences[select]
 
 if __name__ == '__main__':
     app.run(port=5005, host='0.0.0.0', debug=True)
