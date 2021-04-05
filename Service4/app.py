63541b63d89e5965a266ba8ded5c3236ba703702
Daniel <destruktor@master.us-central1-a.c.peppy-castle-305110.internal>
2021-04-05 11:03:13 +0000

    Updated Service4

--- Service4/app.py
+++ Service4/app.py
@@ -4,10 +4,19 @@ app = Flask(__name__)
 @app.route('/number', methods=['GET'])
 def number():
 
-    response = requests.post(api + '/post/text', 'My Data')
-    print('Response: ', response.text)
+    # response = requests.post(api + '/post/text', 'My Data')
+    # print('Response: ', response.text)
+    # return 4
 
-    return 4
+    response = requests.get('http://api:5000/')
+
+    if response.text == 'Dan':
+        return 'dan.html'
+    else:
+        return 'other.html'
+
+
+
 
 if __name__ == '__main__':
     app.run(port=5050, host='0.0.0.0', debug=True)
