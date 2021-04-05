69570eafc60976391b2bbfeaf1316d6371cb5ece
Daniel <destruktor@master.us-central1-a.c.peppy-castle-305110.internal>
2021-04-05 13:47:18 +0000

    updated app to generate random number

--- Service2/app.py
+++ Service2/app.py
@@ -1,9 +1,19 @@
 from flask import Flask
+from random import randint
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def randomizer():
-    return '2'
+
+    random = randint(1, 100)
+
+    if request.method == 'GET':
+
+        random = randint(1, 15)
+        
+    return f'{random}'
+
+
 
 if __name__ == '__main__':
     app.run(port=5000, host='0.0.0.0', debug=True)
