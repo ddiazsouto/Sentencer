d69537c82470063cbb5b65f774a85947987df9ba
Daniel <destruktor@master.us-central1-a.c.peppy-castle-305110.internal>
2021-04-05 14:51:32 +0000

    Basic GET request

--- Service4/app.py
+++ Service4/app.py
@@ -15,7 +15,7 @@ def middleend():
     #     data_received = request.data.decode('utf-8')      
     #     return Response(data_received)
 
-    return f'Hey, I am talking with Service 2, and its saying: {str(response)}'
+    return f'Hey, I am talking with Service 2, and its saying: {response.text}'
 
 if __name__ == '__main__':
     app.run(port=5050, host='0.0.0.0', debug=True)
