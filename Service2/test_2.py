e7a39de83839c09961d3e089600cf11bc7bc33a7
Daniel <destruktor@master.us-central1-a.c.peppy-castle-305110.internal>
2021-04-05 13:56:44 +0000

    Included test for Service2

---
+++ Service2/test_2.py
@@ -0,0 +1,42 @@
+
+from unittest.mock import patch
+from flask import url_for
+from flask_testing import TestCase
+
+from Service2.app import app
+
+
+
+# pytest
+# pytest --cov=app
+# pytest --cov-config=.coveragec --cov=.
+# pytest --cov=app --cov-report=term-missing
+# pytest --cov . --cov-report html
+
+
+
+class TestBase(TestCase):   # main function to create the app environment
+
+    def create_app(self):   # its configuration
+          
+        return app
+
+       
+
+class TestViews(TestBase):  # This test confirms that the page loads
+
+    def test_home_get(self):
+        response = self.client.get(url_for('randomizer'))
+        self.assertEqual(response.status_code, 200)
+
+
+
+# class TestResponse(TestBase):
+
+#     def test_one(self):
+#     # We will mock a response of 1 and test that we get football returned.
+#         with patch('requests.get') as g:
+#             g.return_value.text = "Dan"
+
+#             response = self.client.get(url_for('middleend'))
+#             self.assertIn(b'dan.html', response.data)
