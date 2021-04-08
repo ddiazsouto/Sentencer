ad02758ae34832441b22452858a345a5273a80e9
Daniel <destruktor@master.us-central1-a.c.peppy-castle-305110.internal>
2021-04-08 10:39:36 +0000

    improved service 3

--- Service3/app.py
+++ Service3/app.py
@@ -4,12 +4,9 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def randomization():
-    sentences = ['It was the craziest thing that my eyes have seen', 'How great', 'Could be better', 'Holly Cow!!', \
-    'Awesome', 'Like a fish in the water', 'Who cares?', 'Yeeah baby!!', 'Not bad at all', 'Fantastic!', 'Nice!']
 
-    select = azar([0, 10], 1)
-
-    return str(sentences[select])
+    mood = azar([0, 1], 1)
+    return expresser(mood)
 
 if __name__ == '__main__':
     app.run(port=5005, host='0.0.0.0', debug=True)
--- Service3/elementae.py
+++ Service3/elementae.py
@@ -16,3 +16,18 @@ def azar(rang, numbers):
         buffer=buffer[0]
 
     return buffer
+
+
+def expresser(mood):
+
+    positive = ['It was the gratest thing that my eyes have yet seen!!', 'How great', 'Absolutely tremendous!!', 'Holly Cow!!', \
+    'Awesome', 'Just like a fish in the water', 'Makes me so happy!', 'Yeeah baby!!', 'Not bad at all', 'Fantastic!', 'Nice!']
+
+    negative = ['Just horrible', 'Do not even try man...', 'Could be better', 'Holly Cow!!', \
+    'Horrendous!!', 'Please, that is enough', 'Who cares?', 'Bad, bad, bad...', 'I better leave now.', 'That is just so baaaad!', 'Can not stand it anymore']
+
+    if mood == 1:
+        return positive[azar([0, 10], 1)]
+    elif mood ==0:
+        return negative[azar([0, 10], 1)]         
+
