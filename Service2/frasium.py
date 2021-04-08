a71d481ff66cfa5a96c4b361af7d74b70f75e711
Daniel <destruktor@master.us-central1-a.c.peppy-castle-305110.internal>
2021-04-08 13:31:24 +0000

    a bit less jsonified, got excited

--- Service2/frasium.py
+++ Service2/frasium.py
@@ -26,10 +26,9 @@ def binary():
 
 def phraser():
 
-    positive = ['It was the gratest thing that my eyes have yet seen!!', 'How great', 'Absolutely tremendous!!', 'Holly Cow!!', \
-    'Awesome', 'Just like a fish in the water', 'Makes me so happy!', 'Yeeah baby!!', 'Not bad at all', 'Fantastic!', 'Nice!']
+    pronoun = ['I', 'You', 'He', 'She', 'It',  'We', 'You', 'They']
 
-    negative = ['Just horrible', 'Do not even try man...', 'Could be better', 'Holly Cow!!', \
+    verb = ['jumping', 'watching', 'running', 'skipping', \
     'Horrendous!!', 'Please, that is enough', 'Who cares?', 'Bad, bad, bad...', 'I better leave now.', 'That is just so baaaad!', 'Can not stand it anymore']
 
     output = dict()
