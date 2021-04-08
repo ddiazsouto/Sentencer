a7f487bb843c5668055195eee646a241397ab101
Daniel <destruktor@master.us-central1-a.c.peppy-castle-305110.internal>
2021-04-08 13:09:58 +0000

    Update service 2

--- Service2/frasium.py
+++ Service2/frasium.py
@@ -1,3 +1,21 @@
+from random import randint
+
+
+def azar(rang, numbers):
+
+    buffer = []
+    lowl  = rang[0]
+    highl = rang[1]
+
+    for i in range(numbers):
+        buffer.append(randint(lowl, highl))
+        
+    if len(buffer)==1:
+        buffer=buffer[0]
+
+    return buffer
+
+
 def binary():
     random = azar([1, 1000], 1)
     
