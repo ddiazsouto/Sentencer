5fcff0371cd25f2967e0a8ffc3b1d0df0f6a9244
Daniel <destruktor@master.us-central1-a.c.peppy-castle-305110.internal>
2021-04-08 17:19:29 +0000

    Update

--- testing.sh
+++ testing.sh
@@ -5,4 +5,7 @@ source venv/bin/activate
 pip3 install -r requiretest.txt
 
 
-python3 -m pytest --cov=. --cov-report=term-missing
+python3 -m pytest --cov=Service1 --cov-report=term-missing
+python3 -m pytest --cov=Service2 --cov-report=term-missing
+python3 -m pytest --cov=Service3 --cov-report=term-missing
+python3 -m pytest --cov=Service4 --cov-report=term-missing
