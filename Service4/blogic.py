7253035ddf14afb59778a256d09d21a05119e6bc
Daniel <destruktor@master.us-central1-a.c.peppy-castle-305110.internal>
2021-04-09 08:03:32 +0000

    making code more solid

--- Service4/blogic.py
+++ Service4/blogic.py
@@ -28,8 +28,8 @@ class BeLogic():
         
         def tones(mood):
         
-            if mood == 1 or mood == 0:
-                happy = ["XYXY00", "XY5500", "XY00XY", "XY0000"]
+            if mood == 1:
+                happy = ["XYXY00", "XY5500", "XY00XY", "XY0000"]   #   XY00XY seems sad
                 return happy[azar([0, 3], 1)]
             else:
                 sad = ["0000XY", "00XY00", "00XYXY", "0XYXY0", "XYXYXY"]
