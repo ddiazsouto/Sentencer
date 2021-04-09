862a3cf441cecb2dd1c91ee2aa974ddff6851ef0
Daniel <destruktor@master.us-central1-a.c.peppy-castle-305110.internal>
2021-04-09 08:26:19 +0000

    Improved syntax yet further

--- Service2/frasium.py
+++ Service2/frasium.py
@@ -42,6 +42,12 @@ def phraser(list, question):
 
     output['pronoun'] = pronoun[list[0]]
 
+    if output['pronoun'] in ['He', 'She', 'It'] and output['aux'] != ['Does', 'Do']:
+        
+        output['verb1'] = verb1[list[1]] + 's'          # Here we make a condition for when the pronoun is third person
+    else:                                               #   so we add the 's' in the end of the verb
+        output['verb1'] = verb1[list[1]]
+
     output['verb2'] = verb2[list[2]]
     output['preposition'] = preposition[list[3]]
 
@@ -52,11 +58,5 @@ def phraser(list, question):
         output['aux'] = 'Do'  
         if output['pronoun'] != 'I':
             output['pronoun'] = output['pronoun'].lower()
-    else:
-        if output['pronoun'] in ['He', 'She', 'It']:
-            output['verb1'] = verb1[list[1]] + 's'          # Here we make a condition for when the pronoun is third person
-        else:                                               #   so we add the 's' in the end of the verb
-            output['verb1'] = verb1[list[1]]
     
-
     return output  
