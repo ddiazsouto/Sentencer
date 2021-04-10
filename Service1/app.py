8ccc7224daf4977a85978f364543a133fb58b362
Daniel <destruktor@master.us-central1-a.c.peppy-castle-305110.internal>
2021-04-10 12:57:16 +0000

    Printing out HOSTNAME

--- Service1/app.py
+++ Service1/app.py
@@ -8,7 +8,7 @@ from os import getenv
 def main():
     template='main.html'
 
-    host = getenv("LOCAL_HOST")
+    host = getenv("HOSTNAME")
     
 
     gotit = requests.get('http://10.128.0.54:5050/').json()
