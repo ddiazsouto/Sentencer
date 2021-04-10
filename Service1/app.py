4bd6d3174122a115d4bdca0960452fcab5412c07
Daniel <destruktor@master.us-central1-a.c.peppy-castle-305110.internal>
2021-04-10 22:39:19 +0000

    update

--- Service1/app.py
+++ Service1/app.py
@@ -8,30 +8,30 @@ from os import getenv
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
-    template='main.html'
 
-    # host = getenv("HOSTNAME")    
+    # host = getenv("HOSTNAME") 
 
-    # DanSQL().write("CREATE TABLE IF NOT EXISTS some(data int(10), more int(20));")
-
-    #DanSQL().write("INSERT INTO some(data, more) VALUES(1234, 7895);")
-    
-    # if request.method == 'GET':
-    gotit = requests.get('http://10.128.0.54:5050/').json()   
+    msg = 'Click in New Message to Start the fun!!'
+    color = '000000'
 
 
-    color = gotit['color']
-    msg = gotit['sentence']
+    if request.method == 'GET':
+        gotit = requests.get('http://10.128.0.54:5050/').json()   
+        color = gotit['color']
+        msg = gotit['sentence']
+
+
+
     
     
     if request.method == 'POST':
 
-         # DanSQL().write("CREATE TABLE IF NOT EXISTS some(data int(10), more int(20));")
-        DanSQL().write("INSERT INTO some(data, more) VALUES(333, 78095);")
+        DanSQL().write("CREATE TABLE IF NOT EXISTS some(Date TIMESTAMP DEFAULT now(), Sentence VARCHAR(100), id INT NOT NULL AUTO_INCREMENT PRIMARY KEY);")
+        DanSQL().write("INSERT INTO some(data, more) VALUES(000, 987);")
 
 
 
-    return render_template(template, title='Frontend', color=color, var=msg)
+    return render_template('main.html', title='Frontend', color=color, var=msg)
 
 if __name__ == '__main__':
     app.run(port=5500, host='0.0.0.0', debug=True)
