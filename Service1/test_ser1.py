e6b55b850baf33af9cb45f8d7fe1ad12559adcdf
Daniel <destruktor@master.us-central1-a.c.peppy-castle-305110.internal>
2021-04-11 23:29:53 +0000

    Extended testing for database usage

--- Service1/test_ser1.py
+++ Service1/test_ser1.py
@@ -2,6 +2,7 @@
 from unittest.mock import patch
 from flask import url_for
 from flask_testing import TestCase
+from things import DanSQL
 
 from app import app
 
@@ -32,6 +33,48 @@ class TestViews(TestBase):  # This test confirms that the page loads
     def test_data_get(self):
         response = self.client.get(url_for('data'))
         self.assertEqual(response.status_code, 200)
+
+
+
+
+class MyAlchemy():
+
+    def connects():
+
+        try:
+            attempt = DanSQL('mysql')
+            attempt.off()
+            return True
+        
+        except: return False
+               
+
+    def creates(value):
+
+                
+        DanSQL('master').write('CREATE DATABASE IF NOT EXISTS testbase;')      
+
+        DanSQL('testbase').write('CREATE TABLE IF NOT EXISTS Test(column1 VARCHAR(10));')
+        DanSQL('testbase').write(f'INSERT INTO Test(column1) values({str(value)});')        
+        var = DanSQL('testbase').get('SELECT * FROM Test;')
+
+        DanSQL('master').write('DROP DATABASE testbase;')
+
+        
+
+        return str(var)    
+
+
+
+
+def test4():                            # Is the conection with the database successful ?
+    assert MyAlchemy.connects() == True
+
+def test5():                            # Checks that the object can interact with the database using an integer
+    assert '127' in MyAlchemy.creates(127)
+
+def test6():                            # Checks that the object can interact with the database using a string
+    assert 'Dan' in MyAlchemy.creates("'Dan'")
 
 
 
